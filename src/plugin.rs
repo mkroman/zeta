033ac5c9caf2c8f98802380223c3f28498d06c6d
Mikkel Kroman <mk@maero.dk>
2024-12-15 16:29:32 +0100

    Add basic reference plugins

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,23 +1,63 @@
+use std::time::Duration;
+
 use async_trait::async_trait;
 use irc::client::Client;
 use irc::proto::{Command, Message};
+use reqwest::header;
+use tokio::runtime::Handle;
+use tokio::time::Instant;
 use tracing::{debug, trace};
+use url::Url;
 
 use crate::Error;
 
 /// The name of a plugin.
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Name(&'static str);
 /// The author of a plugin.
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Author(&'static str);
 /// The version of a plugin.
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Version(&'static str);
 
-pub struct GoogleSearch {}
+pub struct Health;
+
+pub struct GoogleSearch {
+    client: reqwest::Client,
+}
+
+impl GoogleSearch {
+    async fn search(&self, _query: &str) -> Result<(), Error> {
+        let url = Url::parse("https://www.google.dk/search?hl=en").unwrap();
+        let res = self.client.get(url).send().await.unwrap();
+
+        debug!(?res, "performed google search");
+
+        Ok(())
+    }
+}
+
+const USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0";
 
 #[async_trait]
 impl Plugin for GoogleSearch {
     fn new() -> GoogleSearch {
-        GoogleSearch {}
+        let mut headers = header::HeaderMap::new();
+        headers.insert(
+            "Accept-Language",
+            header::HeaderValue::from_static("en-US,en;q=0.5"),
+        );
+        headers.insert("Accept-Charset", header::HeaderValue::from_static("utf-8"));
+
+        let client = reqwest::Client::builder()
+            .user_agent(USER_AGENT)
+            .default_headers(headers)
+            .timeout(Duration::from_secs(30))
+            .build()
+            .unwrap();
+
+        GoogleSearch { client }
     }
 
     fn name() -> Name {
@@ -38,6 +78,54 @@ impl Plugin for GoogleSearch {
                 debug!("user requested google search");
 
                 client.send_privmsg(channel, format!("searching for {query}"))?;
+
+                let now = Instant::now();
+
+                if let Ok(_res) = self.search(query).await {
+                    client.send_privmsg(
+                        channel,
+                        format!("got results in {}ms", now.elapsed().as_millis()),
+                    )?;
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl Plugin for Health {
+    fn new() -> Health {
+        Health
+    }
+
+    fn name() -> Name {
+        Name("health")
+    }
+
+    fn author() -> Author {
+        Author("Mikkel Kroman <mk@maero.dk>")
+    }
+
+    fn version() -> Version {
+        Version("0.1")
+    }
+
+    async fn handle_message(&self, message: &Message, client: &Client) -> Result<(), Error> {
+        if let Command::PRIVMSG(ref channel, ref message) = message.command {
+            if message.starts_with(".health") {
+                let metrics = Handle::current().metrics();
+
+                let num_workers = metrics.num_workers();
+                let num_alive_tasks = metrics.num_alive_tasks();
+
+                client.send_privmsg(
+                    channel,
+                    format!(
+                        "\x0310>\x0f\x02 Health:\x02\x0310 Worker threads:\x0f {num_workers}\x0310 Alive tasks:\x0f {num_alive_tasks}",
+                    ),
+                )?;
             }
         }
 
@@ -88,6 +176,7 @@ impl Registry {
         let mut registry = Self::new();
         trace!("Registering plugins");
         registry.register::<GoogleSearch>();
+        registry.register::<Health>();
 
         let num_plugins = registry.plugins.len();
         trace!(%num_plugins, "Done registering plugins");
