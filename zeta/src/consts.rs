ec9d525ffba5385d549dfc9761fc56087ce2ac99
Mikkel Kroman <mk@maero.dk>
2025-08-16 17:20:06 +0200

    Add YouTube and GeoIP plugins

--- zeta/src/consts.rs
+++ zeta/src/consts.rs
@@ -1,2 +1,8 @@
-pub const USER_AGENT: &str =
-    "Mozilla/5.0 (X11; Linux x86_64; rv:138.0) Gecko/20100101 Firefox/138.0";
+use std::time::Duration;
+
+/// The `User-Agent` header to send when issuing HTTP requests.
+pub const HTTP_USER_AGENT: &str =
+    "Mozilla/5.0 (X11; Linux x86_64; rv:141.0) Gecko/20100101 Firefox/141.0";
+
+/// The duration before a HTTP request times out.
+pub const HTTP_TIMEOUT: Duration = Duration::from_secs(30);
---
+++ zeta/src/plugin/geoip.rs
@@ -0,0 +1,212 @@
+use std::fmt::Display;
+
+use argh::FromArgs;
+use async_trait::async_trait;
+use irc::client::Client;
+use irc::proto::{Command, Message};
+use reqwest::redirect::Policy;
+use reqwest::StatusCode;
+use serde::Deserialize;
+use thiserror::Error;
+use tracing::{error, info};
+
+use crate::consts::HTTP_TIMEOUT;
+use crate::Error as ZetaError;
+
+use super::{Author, Name, Plugin, Version};
+
+const BASE_URL: &str = "https://api.ip2location.io";
+
+pub struct GeoIp {
+    pub client: reqwest::Client,
+    api_key: String,
+}
+
+#[derive(Default)]
+pub struct LookupResult(IpInfo);
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("could not parse arguments")]
+    ParseArguments,
+    #[error("could not deserialize response: {0}")]
+    Deserialize(#[source] reqwest::Error),
+    #[error("http request failed")]
+    Request(#[from] reqwest::Error),
+    #[error("invalid IP address or domain")]
+    InvalidInput,
+}
+
+/// Geographical lookup utility based on IP address
+#[derive(FromArgs, Debug)]
+pub struct Opts {
+    /// the name of the domain to look to look up
+    #[argh(positional)]
+    name: String,
+}
+
+/// Represents geographical and network information for an IP address.
+/// This struct is designed to be deserialized from a JSON response
+/// providing details like location, timezone, and ASN data.
+#[allow(unused)]
+#[derive(Debug, Deserialize, Default)]
+pub struct IpInfo {
+    /// The IP address.
+    pub ip: String,
+    /// The two-letter ISO 3166-1 alpha-2 country code.
+    pub country_code: String,
+    /// The name of the country.
+    pub country_name: String,
+    /// The name of the region or state.
+    pub region_name: String,
+    /// The name of the city.
+    pub city_name: String,
+    /// The geographical latitude.
+    pub latitude: f64,
+    /// The geographical longitude.
+    pub longitude: f64,
+    /// The postal or zip code.
+    pub zip_code: String,
+    /// The time zone offset from UTC.
+    pub time_zone: String,
+    /// The Autonomous System Number (ASN).
+    pub asn: String,
+    /// The name of the entity that owns the Autonomous System.
+    #[serde(rename = "as")]
+    pub asn_name: String,
+    /// Indicates whether the IP address is a known proxy.
+    pub is_proxy: bool,
+}
+
+#[async_trait]
+impl Plugin for GeoIp {
+    fn new() -> GeoIp {
+        let client = reqwest::Client::builder()
+            .redirect(Policy::none())
+            .timeout(HTTP_TIMEOUT)
+            .build()
+            .expect("could not build http client");
+
+        let api_key =
+            std::env::var("GEOIP_API_KEY").expect("missing GEOIP_API_KEY environment variable");
+
+        GeoIp { client, api_key }
+    }
+
+    fn name() -> Name {
+        Name("geoip")
+    }
+
+    fn author() -> Author {
+        Author("Mikkel Kroman <mk@maero.dk>")
+    }
+
+    fn version() -> Version {
+        Version("0.1")
+    }
+
+    async fn handle_message(&self, message: &Message, client: &Client) -> Result<(), ZetaError> {
+        if let Command::PRIVMSG(ref channel, ref message) = message.command {
+            if let Some(args) = message.strip_prefix(".geoip ") {
+                let sub_args = shlex::split(args)
+                    .ok_or_else(|| ZetaError::PluginError(Box::new(Error::ParseArguments)))?;
+                let sub_args_ref = sub_args.iter().map(String::as_ref).collect::<Vec<_>>();
+
+                match Opts::from_args(&[".geoip"], &sub_args_ref) {
+                    Ok(opts) => match self.resolve(&opts.name).await {
+                        Ok(result) => {
+                            for line in result.to_string().lines() {
+                                client
+                                    .send_privmsg(channel, line)
+                                    .map_err(ZetaError::IrcClientError)?;
+                            }
+                        }
+                        Err(err) => {
+                            client
+                                .send_privmsg(
+                                    channel,
+                                    format!("\x0310>\x03\x02 GeoIP:\x02\x0310 {err}"),
+                                )
+                                .map_err(ZetaError::IrcClientError)?;
+                        }
+                    },
+                    Err(err) => {
+                        client
+                            .send_privmsg(
+                                channel,
+                                format!("\x0310>\x03\x02 GeoIP:\x02\x0310 {}", err.output),
+                            )
+                            .map_err(ZetaError::IrcClientError)?;
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl Display for IpInfo {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut parts = Vec::new();
+
+        if !self.asn_name.is_empty() {
+            parts.push(format!("AS:\x03 {}\x0310", self.asn_name));
+        }
+
+        if !self.asn.is_empty() {
+            parts.push(format!("ASN:\x03 {}\x0310", self.asn));
+        }
+
+        if !self.country_name.is_empty() {
+            parts.push(format!("Country:\x03 {}\x0310", self.country_name));
+        }
+
+        if !self.region_name.is_empty() {
+            parts.push(format!("Region:\x03 {}\x0310", self.region_name));
+        }
+
+        if !self.city_name.is_empty() {
+            parts.push(format!("City:\x03 {}\x0310", self.city_name));
+        }
+
+        if parts.is_empty() {
+            write!(fmt, "No location data available")
+        } else {
+            write!(fmt, "{}", parts.join("\x0310 "))
+        }
+    }
+}
+
+impl Display for LookupResult {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let info = &self.0;
+        let ip = &info.ip;
+
+        write!(
+            fmt,
+            "\x0310>\x03\x02 GeoIP\x02\x0310 (\x0f{ip}\x0310): {info}"
+        )
+    }
+}
+
+impl GeoIp {
+    pub async fn resolve(&self, name: &str) -> Result<LookupResult, Error> {
+        let params = [("ip", name), ("key", &self.api_key), ("format", "json")];
+        let request = self.client.get(BASE_URL).query(&params);
+        let response = request.send().await?;
+
+        match response.error_for_status() {
+            Ok(response) => {
+                info!(response = %response.status(), "resolved");
+                let info: IpInfo = response.json().await.map_err(Error::Deserialize)?;
+                Ok(LookupResult(info))
+            }
+            Err(err) => {
+                error!(?err, %name, "error when querying for geoip");
+
+                Err(err.into())
+            }
+        }
+    }
+}
