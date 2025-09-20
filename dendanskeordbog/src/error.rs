a57955ac9ba03ba546c26dcccf6722b326f5047a
mkroman <173308+mkroman@users.noreply.github.com>
2025-09-20 10:04:10 +0200

    Add dendanskeordbog crate and plugin

---
+++ dendanskeordbog/src/error.rs
@@ -0,0 +1,15 @@
+use thiserror::Error;
+
+/// Error.
+#[derive(Debug, Error)]
+pub enum Error {
+    #[cfg(feature = "client")]
+    #[error("could not construct http client: {0}")]
+    BuildClient(#[source] reqwest::Error),
+    #[cfg(feature = "client")]
+    #[error("request error: {0}")]
+    Request(#[source] reqwest::Error),
+    #[cfg(feature = "client")]
+    #[error("could not find element using selector: {0}")]
+    MissingElement(&'static str),
+}
