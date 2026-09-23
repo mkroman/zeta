7bdfda8c34412bac40c9d35baef6e168fcd960e7
mkroman <173308+mkroman@users.noreply.github.com>
2026-09-23 11:49:34 +0200

    Terminate the wrapped warn! arm with a semicolon
    Wrapping the arm in braces during the rustfmt pass left a block whose last expression is the warn! call; clippy::semicolon_if_nothing_returned wants the statement terminated.

--- zeta/src/zeta.rs
+++ zeta/src/zeta.rs
@@ -68,7 +68,7 @@ async fn drain_plugin_handles(handles: Vec<(String, JoinHandle<()>)>, grace: Dur
             // The task was cancelled by its own runtime (or this function, below).
             Ok(Err(error)) if error.is_cancelled() => {}
             Ok(Err(error)) => {
-                warn!(plugin = %name, %error, "plugin task panicked while shutting down")
+                warn!(plugin = %name, %error, "plugin task panicked while shutting down");
             }
             Err(_) => {
                 warn!(plugin = %name, "plugin task did not shut down in time; aborting");
